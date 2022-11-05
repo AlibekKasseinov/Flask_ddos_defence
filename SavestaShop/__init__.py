8c55c9b3ba891e9c1186cce82312365f17f0dfd1
alial <ali.altynbekov228@gmail.com>
2022-11-05 23:58:04 +0600

    AAAAAA

--- SavestaShop/__init__.py
+++ SavestaShop/__init__.py
@@ -2,3 +2,13 @@ from flask import Flask, render_template, request, url_for, redirect, abort, ses
 from flask_session import Session
 from OnlineShop.dbaccess import *
 import os
+
+app = Flask(__name__)
+sess = Session()
+
+app.config['SECRET_KEY'] = os.urandom(17)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+sess.init_app(app)
+if __name__=="__main__":
+	app.run(hostname='192.168.43.163')
