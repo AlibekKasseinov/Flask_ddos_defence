9b9d8b972846815eee3274cdd89d49c6e43a12c6
alial <ali.altynbekov228@gmail.com>
2022-11-06 00:02:07 +0600

    A

--- SavestaShop/__init__.py
+++ SavestaShop/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, abort, session
 from flask_session import Session
-from OnlineShop.dbaccess import *
 import os
 
 app = Flask(__name__)
