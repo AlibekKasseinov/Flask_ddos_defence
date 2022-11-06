3a0b67790e84e81b5f3092b2e317030b72a9e8cb
alial <ali.altynbekov228@gmail.com>
2022-11-06 22:31:30 +0600

    edit profile, seller product, profile,

--- SavestaShop/__init__.py
+++ SavestaShop/__init__.py
@@ -68,6 +68,59 @@ def view_profile(id):
                             category=(None if profile_type=="Customer" else categories),
                             my=my)
 
+@app.route("/viewprofile/", methods=["POST", "GET"])
+def profile():
+    if 'userid' not in session:
+        return redirect(url_for('home'))
+    type = "Seller" if session['type']=="Customer" else "Customer"
+    if request.method=="POST":
+        search = request.form['search']
+        results = search_users(search, type)
+        found = len(results)
+        return render_template('profiles.html', id=session['userid'], type=type, after_srch=True, found=found, results=results)
+
+    return render_template('profiles.html', id=session['userid'], type=type, after_srch=False)
+
+@app.route("/viewprofile/<id>/sellerproducts/")
+def seller_products(id):
+    if 'userid' not in session:
+        return redirect(url_for('home'))
+    if session["type"]=="Seller":
+        abort(403)
+    det, categories = fetch_details(id, "Seller")   #details
+    if len(det)==0:
+        abort(404)
+    det = det[0]
+    name=det[1]
+    res = get_seller_products(id)
+    return render_template('seller_products.html', name=name, id=id, results=res)
+
+@app.route("/editprofile/", methods=["POST", "GET"])
+def edit_profile():
+    if 'userid' not in session:
+        return redirect(url_for('home'))
+
+    if request.method=="POST":
+        data = request.form
+        update_details(data, session['userid'], session['type'])
+        return redirect(url_for('view_profile', id=session['userid']))
+
+    if request.method=="GET":
+        userid = session["userid"]
+        type = session["type"]
+        det, _ = fetch_details(userid, type)
+        det = det[0]
+        return render_template("edit_profile.html",
+                                type=type,
+                                name=det[1],
+                                email=det[2],
+                                phone=det[3],
+                                area=det[4],
+                                locality=det[5],
+                                city=det[6],
+                                state=det[7],
+                                country=det[8],
+                                zip=det[9])
 
 app.config['SECRET_KEY'] = os.urandom(17)
 app.config['SESSION_TYPE'] = 'filesystem'
