69a4013d2497192c4e4c159c02523ac572997a8f
alial <ali.altynbekov228@gmail.com>
2022-11-06 20:30:23 +0600

    first def

--- SavestaShop/__init__.py
+++ SavestaShop/__init__.py
@@ -5,6 +5,12 @@ import os
 app = Flask(__name__)
 sess = Session()
 
+@app.route("/")
+def home():
+    if "userid" in session:
+        return render_template("home.html", signedin=True, id=session['userid'], name=session['name'], type=session['type'])
+    return render_template("home.html", signedin=False)
+
 app.config['SECRET_KEY'] = os.urandom(17)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
