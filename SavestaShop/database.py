82fade8461bc7aed9fa08a842ee59c46a11d903f
darkryzz1 <82153601+darkryzz1@users.noreply.github.com>
2022-11-06 22:37:21 +0600

    password for db

--- SavestaShop/database.py
+++ SavestaShop/database.py
@@ -10,6 +10,72 @@ def gen_custID():
     id = "ID"+"0"*(7-len(custnum))+custnum
     return id
 
+
+def add_user(data):
+    conn = sqlite3.connect("OnlineShop/onlineshop.db")
+    cur = conn.cursor()
+    email = data["email"]
+    if data['type']=="Customer":
+        a = cur.execute("SELECT * FROM customer WHERE email=?", (email,))
+    elif data['type']=="Seller":
+        a = cur.execute("SELECT * FROM seller WHERE email=?", (email,))
+    if len(list(a))!=0:
+        return False
+    tup = ( data["name"],
+            data["email"],
+            data["phone"],
+            data["area"],
+            data["locality"],
+            data["city"],
+            data["state"],
+            data["country"],
+            data["zip"],
+            data["password"])
+    if data['type']=="Customer":
+        cur.execute("INSERT INTO customer VALUES (?,?,?,?,?,?,?,?,?,?,?)",(gen_custID(), *tup))
+    elif data['type']=="Seller":
+        cur.execute("INSERT INTO seller VALUES (?,?,?,?,?,?,?,?,?,?,?)", (gen_sellID(), *tup))
+    conn.commit()
+    conn.close()
+    return True
+
+def auth_user(data):
+    conn = sqlite3.connect("OnlineShop/onlineshop.db")
+    cur = conn.cursor()
+    type = data["type"]
+    email = data["email"]
+    password = data["password"]
+    if type=="Customer":
+        a = cur.execute("SELECT custID, name FROM customer WHERE email=? AND password=?", (email, password))
+    elif type=="Seller":
+        a = cur.execute("SELECT sellID, name FROM seller WHERE email=? AND password=?", (email, password))
+    a = list(a)
+    conn.close()
+    if len(a)==0:
+        return False
+    return a[0]
+
+def check_psswd(psswd, userid, type):
+    conn = sqlite3.connect("OnlineShop/onlineshop.db")
+    cur = conn.cursor()
+    if type=="Customer":
+        a = cur.execute("SELECT password FROM customer WHERE custID=?", (userid,))
+    elif type=="Seller":
+        a = cur.execute("SELECT password FROM seller WHERE sellID=?", (userid,))
+    real_psswd = list(a)[0][0]
+    conn.close()
+    return psswd==real_psswd
+
+def set_psswd(psswd, userid, type):
+    conn = sqlite3.connect("OnlineShop/onlineshop.db")
+    cur = conn.cursor()
+    if type=="Customer":
+        a = cur.execute("UPDATE customer SET password=? WHERE custID=?", (psswd, userid))
+    elif type=="Seller":
+        a = cur.execute("UPDATE seller SET password=? WHERE sellID=?", (psswd, userid))
+    conn.commit()
+    conn.close()
+
 def check_psswd(psswd, userid, type):
     conn = sqlite3.connect("SavestaShop/database.db")
     cur = conn.cursor()
