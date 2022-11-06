dc91b7563f41ecf70f0ada1f893f9183d63cd77a
alial <ali.altynbekov228@gmail.com>
2022-11-06 21:14:08 +0600

    viewprofile, logout, login

--- SavestaShop/__init__.py
+++ SavestaShop/__init__.py
@@ -11,6 +11,55 @@ def home():
         return render_template("home.html", signedin=True, id=session['userid'], name=session['name'], type=session['type'])
     return render_template("home.html", signedin=False)
 
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        data = request.form
+        userdat = auth_user(data)
+        if userdat:
+            session["userid"] = userdat[0]
+            session["name"] = userdat[1]
+            session["type"] = data["type"]
+            return redirect(url_for('home'))
+        return render_template("login.html", err=True)
+    return render_template("login.html", err=False)
+
+@app.route("/logout/")
+def logout():
+    session.pop('userid')
+    session.pop('name')
+    session.pop('type')
+    return redirect(url_for('home'))
+
+@app.route("/viewprofile/<id>/")
+def view_profile(id):
+    if 'userid' not in session:
+        return redirect(url_for('home'))
+    userid = session["userid"]
+    type = session["type"]
+    my = True if userid==id else False
+    if not my: profile_type = "Customer" if type=="Seller" else "Seller"
+    else: profile_type = type
+
+    det, categories = fetch_details(id, profile_type)   #details
+    if len(det)==0:
+        abort(404)
+    det = det[0]
+    return render_template("view_profile.html",
+                            type=profile_type,
+                            name=det[1],
+                            email=det[2],
+                            phone=det[3],
+                            area=det[4],
+                            locality=det[5],
+                            city=det[6],
+                            state=det[7],
+                            country=det[8],
+                            zip=det[9],
+                            category=(None if profile_type=="Customer" else categories),
+                            my=my)
+
+
 app.config['SECRET_KEY'] = os.urandom(17)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
